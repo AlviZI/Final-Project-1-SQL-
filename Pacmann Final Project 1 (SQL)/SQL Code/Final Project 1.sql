6f5667ca9110c244f7912508229709d085646e57
Alvi ZI <66658329+AlviZI@users.noreply.github.com>
2023-07-03 23:23:12 +0700

    Update 030702023
    Import sudah bisa dan menyelesaikan test ke 2

--- Pacmann Final Project 1 (SQL)/SQL Code/Final Project 1.sql
+++ Pacmann Final Project 1 (SQL)/SQL Code/Final Project 1.sql
@@ -1,21 +1,52 @@
 --------------------------------------------------- Membuat Tabel ---------------------------------------------------------------------
-CREATE TABLE car_product (
-  product_id INTEGER NOT NULL PRIMARY KEY,
-  brand VARCHAR NOT NULL,
-  model VARCHAR NOT NULL,
-  body_type VARCHAR NOT NULL,
-  year DATE NOT NULL,
-  price INTEGER NOT NULL
+CREATE TABLE public.car_product
+(
+    product_id integer NOT NULL,
+    brand character varying NOT NULL,
+    model character varying NOT NULL,
+    body_type character varying NOT NULL,
+    year integer NOT NULL,
+    price integer NOT NULL,
+    PRIMARY KEY (product_id),
+    CONSTRAINT product_id UNIQUE (product_id)
 );
 
-CREATE TABLE city (
-  kota_id INTEGER NOT NULL PRIMARY KEY,
-  nama_kota VARCHAR NOT NULL,
-  latitude DECIMAL NOT NULL,
-  longitude DECIMAL NOT NULL
+CREATE TABLE public.city 
+(
+  kota_id integer NOT NULL,
+  nama_kota character varying COLLATE pg_catalog."default" NOT NULL,
+  latitude character varying COLLATE pg_catalog."default" NOT NULL,
+  longitude character varying COLLATE pg_catalog."default" NOT NULL,
+  CONSTRAINT city_pkey PRIMARY KEY (kota_id),
+  CONSTRAINT city_kota_id_key UNIQUE (kota_id)
 );
 
------- Import
-COPY car_product 
-FROM 'C:\Users\lenovo\Documents\GitHub\Final-Project-1-SQL-\Pacmann Final Project 1 (SQL)\Dummy Data\car_product.csv' 
-DELIMITER ',' CSV HEADER;
+CREATE TABLE public.user
+(
+    user_id integer NOT NULL,
+    nama character varying COLLATE pg_catalog."default" NOT NULL,
+    kontak character varying COLLATE pg_catalog."default" NOT NULL,
+    domisili character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT user_pkey PRIMARY KEY (user_id)
+);
+
+---------------------------------------------- Langkah 3 ------------------------------------------------------
+SELECT * FROM car_product;
+
+SELECT *
+FROM car_product
+WHERE year >= 2015;
+
+SELECT *
+FROM car_product
+WHERE year <= 2015 AND brand ILIKE '%Chevrolet%'
+ORDER BY price ASC
+LIMIT 2;
+
+SELECT *
+FROM car_product  cp
+JOIN city c ON cp.city_id = c.kota_id
+WHERE cp.year <= 2015
+  AND c.kota_id = 12 
+ORDER BY SQRT(POWER(cp.latitude - c.latitude, 2) + POWER(cp.longitude - c.longitude, 2))
+LIMIT 1;
